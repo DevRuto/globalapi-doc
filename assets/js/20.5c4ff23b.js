9810aaaaa2a0f5cba5126be5163313cb240e184d
DevRuto <20764435+DevRuto@users.noreply.github.com>
2020-09-08 00:21:26 +0000

    Deploying to gh-pages from  @ add77ebed8c8f96efa5395975904e30bb5668180 🚀

---
+++ assets/js/20.5c4ff23b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{443:function(e,t,n){"use strict";n.r(t);var o={name:"Redirect",props:{to:{type:String,required:!0}},beforeMount(){document.location.replace(this.to)}},r=n(3),i=Object(r.a)(o,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);t.default=i.exports}}]);
